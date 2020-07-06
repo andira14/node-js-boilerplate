fa7ed511a50636ec210531b1624dbf8b463a5b83
ringga.gustavino <ringga.gustavino@gmail.com>
2020-07-07 01:06:28 +0700

    Setup initialization

---
+++ config/express.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+import express from 'express';
+import route from '../server/routes';
+import { Response } from '../server/class/util/response.class';
+
+const app = express();
+
+const baseUrl = '/api'
+
+app.use(baseUrl, route);
+
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    return next(err.stack);
+});
+
+// error handler, send stacktrace only during development
+app.use((err, _, res, next) => {
+    if(err){
+        const response = new Response(res);
+
+        return response.systemError(err);
+    }
+    return next();
+});
+
+// catch 404 and forward to error handler
+app.use((_, res) => {
+    const response = new Response(res);
+
+    return response.apiNotFound();
+});
+
+export default app;
