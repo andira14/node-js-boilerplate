1327a1f7fe47e4baa24210e2c0d861877ee4ed0d
ringga.gustavino <ringga.gustavino@gmail.com>
2020-07-07 01:47:53 +0700

    Initial commit

---
+++ server/misc/misc.js
@@ -0,0 +1,16 @@
+const simplePagination = (page = 1, row = 10) => {
+  const pagination = {
+    page,
+    row,
+  };
+  if (row) {
+    pagination.row = row;
+  }
+  if (page) {
+    pagination.page = (page - 1) * pagination.row;
+  }
+};
+
+export default {
+  simplePagination,
+};
