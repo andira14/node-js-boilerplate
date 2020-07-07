32ecf9f86615a556d1a6ec0cda61f03de0cd91b7
ringga.gustavino <ringga.gustavino@gmail.com>
2020-07-08 05:17:54 +0700

    Fixed nodemon restart

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const {
-  series, src, dest,
+  series, src, dest, task,
 } = require('gulp');
 const babel = require('gulp-babel');
 const nodemon = require('gulp-nodemon');
@@ -8,26 +8,31 @@ const del = require('delete');
 const jsFile = ['./**/*.js', '!./node_modules/**', '!./.history/**'];
 const nonJsFile = ['./package.json', './.sequelizerc'];
 
-const clean = (done) => {
+task('clean', (done) => {
   del.sync('./dist/');
   return done();
-};
+});
 
-const runBabel = () => src(jsFile)
+task('runBabel', () => src(jsFile)
   .pipe(babel({
-    presets: ['@babel/env'],
+    presets: [['@babel/env', {
+      useBuiltIns: 'usage',
+      corejs: 3,
+    }]],
     ignore: ['gulpfile.js'],
   }))
-  .pipe(dest('dist'));
+  .pipe(dest('dist')));
 
-const copy = () => src(nonJsFile)
-  .pipe(dest('./dist/'));
+task('copy', () => src(nonJsFile)
+  .pipe(dest('./dist/')));
 
 const serve = (done) => {
   nodemon({
     script: './dist/index.js',
+    tasks: ['clean', 'runBabel', 'copy'],
+    ignore: ['node_modules/**', 'dist/**', '.history/**'],
   });
   return done();
 };
 
-exports.default = series(clean, runBabel, copy, serve);
+exports.default = series('clean', 'runBabel', 'copy', serve);
